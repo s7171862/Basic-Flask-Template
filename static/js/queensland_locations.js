64971f47a2c135a5a565817b5165ef486b881175
s7171862 <s7171862@terrace.qld.edu.au>
2026-09-01 16:52:32 +1000

    Search and Back to dashboard updates

--- static/js/queensland_locations.js
+++ static/js/queensland_locations.js
@@ -1,32 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const cityLgas = {
-        'Brisbane': 'BRISBANE CITY',
-        'Gold Coast': 'GOLD COAST CITY',
-        'Sunshine Coast': 'SUNSHINE COAST REGIONAL'
+    const queenslandLocations = {
+        'Brisbane': ['Acacia Ridge', 'Albion', 'Algester', 'Annerley', 'Ascot', 'Aspley', 'Auchenflower', 'Bald Hills', 'Banyo', 'Bardon', 'Bellbowrie', 'Belmont', 'Boondall', 'Bowen Hills', 'Bracken Ridge', 'Bridgeman Downs', 'Brisbane City', 'Bulimba', 'Calamvale', 'Camp Hill', 'Cannon Hill', 'Carina', 'Carindale', 'Carseldine', 'Chapel Hill', 'Chermside', 'Chermside West', 'Clayfield', 'Coorparoo', 'Darra', 'Deagon', 'Doolandella', 'Drewvale', 'Durack', 'East Brisbane', 'Eight Mile Plains', 'Enoggera', 'Everton Park', 'Fairfield', 'Ferny Grove', 'Fig Tree Pocket', 'Fortitude Valley', 'Gaythorne', 'Geebung', 'Gordon Park', 'Grange', 'Greenslopes', 'Gumdale', 'Hamilton', 'Hawthorne', 'Hemmant', 'Hendra', 'Highgate Hill', 'Holland Park', 'Indooroopilly', 'Jamboree Heights', 'Kangaroo Point', 'Karana Downs', 'Karawatha', 'Kedron', 'Kelvin Grove', 'Kenmore', 'Kippa-Ring', 'Kuraby', 'Lota', 'Lutwyche', 'Macgregor', 'Manly', 'Mansfield', 'McDowall', 'Mildmay', 'Milton', 'Mitchelton', 'Moggill', 'Moorooka', 'Morningside', 'Mount Coot-tha', 'Mount Gravatt', 'Murarrie', 'Nathan', 'New Farm', 'Newmarket', 'Newstead', 'Nudgee', 'Nundah', 'Paddington', 'Parkinson', 'Petrie Terrace', 'Pinkenba', 'Port of Brisbane', 'Pullenvale', 'Ransome', 'Red Hill', 'Richlands', 'Rochedale', 'Runcorn', 'Salisbury', 'Sandgate', 'Seven Hills', 'Sherwood', 'Shorncliffe', 'Sinnamon Park', 'South Brisbane', 'Spring Hill', 'St Lucia', 'Stafford', 'Stretton', 'Sumner', 'Sunnybank', 'Sunnybank Hills', 'Taringa', 'Teneriffe', 'The Gap', 'Toowong', 'Upper Brookfield', 'Virginia', 'Wacol', 'West End', 'Wilston', 'Windsor', 'Wishart', 'Wolston', 'Wynnum', 'Wynnum West', 'Yeerongpilly', 'Yeronga', 'Zillmere'],
+        'Gold Coast': ['Advancetown', 'Arundel', 'Ashmore', 'Benowa', 'Biggera Waters', 'Bilinga', 'Bonogin', 'Broadbeach', 'Broadbeach Waters', 'Burleigh Heads', 'Burleigh Waters', 'Bundall', 'Carrara', 'Chevron Island', 'Chirn Park', 'Coombabah', 'Coolangatta', 'Coomera', 'Currumbin', 'Currumbin Waters', 'Elanora', 'Gaven', 'Gilston', 'Helensvale', 'Highland Park', 'Hollywell', 'Hope Island', 'Jacobs Well', 'Kingsholme', 'Labrador', 'Lower Beechmont', 'Maudsland', 'Mermaid Beach', 'Mermaid Waters', 'Miami', 'Molendinar', 'Mudgeeraba', 'Nerang', 'Nerang South', 'Ninderry', 'Nobby Beach', 'North Tamborine', 'Ormeau', 'Ormeau Hills', 'Oxenford', 'Pacific Pines', 'Palm Beach', 'Paradise Point', 'Parkwood', 'Pimpama', 'Reedy Creek', 'Robina', 'Runaway Bay', 'Sanctuary Cove', 'Southport', 'Stapylton', 'Surfers Paradise', 'Tallebudgera', 'Tallebudgera Valley', 'Tugun', 'Upper Coomera', 'Varsity Lakes', 'West Burleigh', 'Willow Vale', 'Wongawallan', 'Worongary', 'Yatala'],
+        'Sunshine Coast': ['Alexandra Headland', 'Baringa', 'Battery Hill', 'Bells Creek', 'Bli Bli', 'Bokarina', 'Boreen Point', 'Buddina', 'Buderim', 'Caloundra', 'Caloundra West', 'Cambroon', 'Chevallum', 'Coolum Beach', 'Cooran', 'Cooroy', 'Currimundi', 'Diddillibah', 'Doonan', 'Eerwah Vale', 'Eumundi', 'Flaxton', 'Forest Glen', 'Glenview', 'Golden Beach', 'Harmony', 'Kawana', 'Kawana Island', 'Kiels Mountain', 'Kings Beach', 'Kuluin', 'Landsborough', 'Little Mountain', 'Mapleton', 'Marcus Beach', 'Maroochydore', 'Minyama', 'Mons', 'Montville', 'Mooloolaba', 'Mountain Creek', 'Mudjimba', 'Nambour', 'Noosa Heads', 'Noosaville', 'Peregian Beach', 'Peregian Springs', 'Pelican Waters', 'Pomona', 'Rosemount', 'Sippy Downs', 'Sunrise Beach', 'Tanawha', 'Tewantin', 'Valdora', 'Warana', 'Weyba Downs', 'Woombye', 'Yandina', 'Yaroomba']
     };
-    const localityApi = 'https://spatial-gis.information.qld.gov.au/arcgis/rest/services/Boundaries/AdministrativeBoundaries/MapServer/2/query';
-    const suburbCache = {};
-
-    async function getSuburbs(city) {
-        if (!city) return [];
-        if (suburbCache[city]) return suburbCache[city];
-        const parameters = new URLSearchParams({
-            f: 'json',
-            where: `lga = '${cityLgas[city]}'`,
-            outFields: 'locality',
-            returnGeometry: 'false',
-            orderByFields: 'locality',
-            resultRecordCount: '1000'
-        });
-        try {
-            const response = await fetch(`${localityApi}?${parameters}`);
-            const data = await response.json();
-            suburbCache[city] = [...new Set((data.features || []).map((feature) => feature.attributes.locality).filter(Boolean))];
-        } catch (error) {
-            suburbCache[city] = [];
-        }
-        return suburbCache[city];
-    }
 
     document.querySelectorAll('[data-location-city]').forEach((citySelect) => {
         const form = citySelect.closest('form');
@@ -37,16 +14,14 @@ document.addEventListener('DOMContentLoaded', () => {
         const firstSuburbLabel = suburbSelect.options[0].text;
 
         citySelect.innerHTML = `<option value="">${firstCityLabel}</option>`;
-        Object.keys(cityLgas).forEach((city) => {
+        Object.keys(queenslandLocations).forEach((city) => {
             const option = new Option(city, city, false, city === selectedCity);
             citySelect.add(option);
         });
 
-        const fillSuburbs = async () => {
+        const fillSuburbs = () => {
             const city = citySelect.value;
-            suburbSelect.disabled = true;
-            suburbSelect.innerHTML = `<option value="">${city ? 'Loading suburbs…' : 'Choose a city first'}</option>`;
-            const suburbs = await getSuburbs(city);
+            const suburbs = queenslandLocations[city] || [];
             suburbSelect.innerHTML = `<option value="">${city ? firstSuburbLabel : 'Choose a city first'}</option>`;
             suburbs.forEach((suburb) => suburbSelect.add(new Option(suburb, suburb, false, suburb === selectedSuburb)));
             suburbSelect.disabled = !city;
